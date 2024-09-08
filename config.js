1da378a1096b86f2a9a21c258542b7b1e7b38075
JaredWright12 <ghoulmange@gmail.com>
2024-09-08 05:06:16 +0000

    commit

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
     "gointerstellar.app": "",
-    localhost: "",
+    localhost: "8080",
   },
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    Glibson: "/Ghoul",
   },
 };
 
